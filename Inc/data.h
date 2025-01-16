566f41a4cb703b030e9d90db768570be08606c37
rrquizon1 <70574862+rrquizon1@users.noreply.github.com>
2025-01-16 20:55:56 +0800

    Add files via upload

---
+++ Inc/data.h
@@ -0,0 +1,14 @@
+/*
+ * data.h
+ *
+ *  Created on: Jan 1, 2025
+ *      Author: Rhodz
+ */
+
+#ifndef INC_DATA_H_
+#define INC_DATA_H_
+
+extern const int g_iDataSize;  // Declaration of the integer
+extern const unsigned char g_pucDataArray[];  // Declaration of the array
+
+#endif /* INC_DATA_H_ */
